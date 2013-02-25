870fc7635c9fd409f1803b6c4542bfac3270dfce
Lincoln Turley <lincoln.turley@gmail.com>
2013-02-25 15:04:00 -0700

    Second test of PHP file - update

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <form action="welcome.php" method="post">
 Name: <input type="text" name="fname">
 Age: <input type="text" name="age">
+Email: <input type="text" name="email">
 <input type="submit">
 </form>
 
